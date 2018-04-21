c8b4f6f3f9719bca73ff9ed25bd62aff556220da
dankuck <dankuck@gmail.com>
2018-04-20 23:33:08 -0400

    Expectations

--- tests/RequirementAnalysisTest.php
+++ tests/RequirementAnalysisTest.php
@@ -13,6 +13,7 @@ class RequirementAnalysisTest extends TestCase
         $one = new RequirementAnalysis([]);
         $this->assertEquals('', $one->line);
         $this->assertFalse($one->has_error);
+        $this->assertFalse($one->has_parse_error);
         $this->assertFalse($one->is_obsolete);
         $this->assertFalse($one->is_incomplete);
         $this->assertFalse($one->has_warning);
@@ -20,16 +21,18 @@ class RequirementAnalysisTest extends TestCase
         $this->assertEquals([], $one->notes);
 
         $two = new RequirementAnalysis([
-            'line' => 'ohai',
-            'has_error' => true,
-            'is_obsolete' => true,
-            'is_incomplete' => true,
-            'has_warning' => true,
-            'is_pending' => true,
-            'notes' => ['yeah'],
+            'line'            => 'ohai',
+            'has_error'       => true,
+            'has_parse_error' => true,
+            'is_obsolete'     => true,
+            'is_incomplete'   => true,
+            'has_warning'     => true,
+            'is_pending'      => true,
+            'notes'           => ['yeah'],
         ]);
         $this->assertEquals('ohai', $two->line);
         $this->assertTrue($two->has_error);
+        $this->assertTrue($two->has_parse_error);
         $this->assertTrue($two->is_obsolete);
         $this->assertTrue($two->is_incomplete);
         $this->assertTrue($two->has_warning);
